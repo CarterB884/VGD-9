898aaac25ed50ef90e09c0aeaf41361b8296a5cf
CarterB884 <carterb884@stemcollegiate.ca>
2025-02-18 12:23:07 -0700

    Update 02-18.cs

--- 02-18.cs
+++ 02-18.cs
@@ -3,4 +3,4 @@ char userOption;
 int gameScore; 
 decimal particlesPerMillion;
 bool processedCustomer;
-// This Unit I learned how to Declare Variables by giving it a data type followed by a name that suits the purpose and follows the rules
+// This Unit I learned how to Declare Variables by giving it a data type followed by a name that suits the purpose and follows the rules. One of the Variable types I learn was called "string" which is used to attach words to a variable.
