c9265f02bcc0eb87869b5583b46cd4c4a84af5fb
CarterB884 <carterb884@stemcollegiate.ca>
2025-02-18 12:16:25 -0700

    Create 02-18.cs

---
+++ 02-18.cs
@@ -0,0 +1,6 @@
+string firstName;
+char userOption;
+int gameScore; 
+decimal particlesPerMillion;
+bool processedCustomer;
+// This Unit I learned how to Declare Variables by giving it a data type followed by a name that suits the purpose and follows the rules
