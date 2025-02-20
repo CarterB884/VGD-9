065be2f4fbfa6f45b9e5608366773629fac619a1
CarterB884 <carterb884@stemcollegiate.ca>
2025-02-20 10:26:11 -0700

    Create 02-20.cs

---
+++ 02-20.cs
@@ -0,0 +1,10 @@
+string firstName;
+firstName = "Caleb";
+Console.WriteLine(firstName);
+firstName = "Christopher";
+Console.WriteLine(firstName);
+firstName = "Demetrius";
+Console.WriteLine(firstName);
+firstName = "Donald";
+Console.WriteLine(firstName);
+// I learned how to assign values to variables and recieve them using the Console.WriteLine(Variable Name); Command
