6bfea42a698b1831ae2cd82252c838ee5f6d5ce6
CarterB884 <carterb884@stemcollegiate.ca>
2025-03-06 10:28:40 -0700

    Create 03-06.cs

---
+++ 03-06.cs
@@ -0,0 +1,12 @@
+//Happy birthday Ghana
+Console.WriteLine(0.25f);
+Console.WriteLine(2.625);
+Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+Console.WriteLine(false);
+Console.WriteLine("123");
+Console.WriteLine(123);
+
+Console.WriteLine("true");
+Console.WriteLine(true);
+// Today I learned about the different variables such as bool, char, int, and double as well as their respective purposes.
