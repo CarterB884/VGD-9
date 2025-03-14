94419f4589b9ca09affbc865367051c3203614bf
CarterB884 <carterb884@stemcollegiate.ca>
2025-03-14 09:07:29 -0600

    Create 03-14.cs

---
+++ 03-14.cs
@@ -0,0 +1,9 @@
+string firstName = "Bob";
+string greeting = "Hello";
+Console.WriteLine(greeting + " " + firstName + "!");
+//In unit 3 I learned about string concatenation by combining string variables with the (+) symbol
+
+string projectName = "First-Project";
+Console.WriteLine($@"C:\Output\{projectName}\Data");
+//In unit 4 I learned how to use interpolation; a method used over string concatenation in some situations to reduce the amount of characters required.
+//I also learned how to combine string interpolation with verbatim literals by combining each of their prefix symbols.
