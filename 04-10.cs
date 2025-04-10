5b46a4125b81a812dfa6d1cb81ebaed836745597
CarterB884 <carterb884@stemcollegiate.ca>
2025-04-10 10:11:36 -0600

    Create 04-10.cs

---
+++ 04-10.cs
@@ -0,0 +1,5 @@
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+// In this lesson I learned how to add integers together using the Console.WriteLine() function. I found out that if needed, the int value will be converted to be a string value and
+that we can use parentheses to define an order of opporations 
